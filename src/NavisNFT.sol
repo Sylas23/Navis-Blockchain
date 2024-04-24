70e85c48715af4cded2deef8ec495c2412a1a37b
eminivince <communicatti@gmail.com>
2024-04-24 09:51:52 +0100

    updated nft

--- src/NavisNFT.sol
+++ src/NavisNFT.sol
@@ -8,13 +8,35 @@ import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 
 
 contract NavisNFT is ERC1155, AccessControl, ERC1155Pausable, ERC1155Burnable, ERC1155Supply {
+    using Counters for Counters.Counter;
+    Counters.Counter public  _tokenIdTracker; 
+    uint256 public constant PREMIUM_ID_OFFSET = 6; // Offsets premium ID from free ID to ensure Premium is non-fungible.
+
+    //
+    
+    mapping(uint=>string[]) nftAbilities;
+
     bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    // mapping(uint256 => string[]) public  shipAbilities;
+    string public name;
+    string public symbol;
+    //mapping (address => )
+    mapping (address=>bool) userHasMinted;
+
+    // Mapping from ship ID to ship abilities
+    mapping(uint256 => string[]) public shipAbilities;
+
+    struct premiumShipData {
+        uint256 id; 
+        string[] shipAbilities;}
+
+    premiumShipData[] public premiumShipAbilities; // Keeps a record of the ship abilities mapped to the id.
 
     address public feeCollector; // Address that collects the token fees
     IERC20 public navisToken;
@@ -25,12 +47,16 @@ contract NavisNFT is ERC1155, AccessControl, ERC1155Pausable, ERC1155Burnable, E
     mapping(uint256 => string) public shipTypeURIs;
 
 
+
+
    constructor(address defaultAdmin, address pauser, address minter, address _feeCollector, address _navisTokenAddress) ERC1155("") {
         _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
         _grantRole(PAUSER_ROLE, pauser);
         _grantRole(MINTER_ROLE, minter);
         feeCollector = _feeCollector;
         navisToken = IERC20(_navisTokenAddress); // Initialize the ERC20 token interface
+        name = "Navis NFT Ship";
+        symbol = "NavisShip";
 
         // Initialize URIs for each ship type, assuming 75 types
         for (uint256 i = 1; i <= 75; i++) {
@@ -38,39 +64,74 @@ contract NavisNFT is ERC1155, AccessControl, ERC1155Pausable, ERC1155Burnable, E
         }
     }
 
+
     function setURI(string memory newuri) public onlyRole(URI_SETTER_ROLE) {
         _setURI(newuri);
     }
+
 
     function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
 
+
     function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
+
 
     function setFeeCollector(address _feeCollector) public onlyRole(DEFAULT_ADMIN_ROLE) {
         feeCollector = _feeCollector;
     }
 
-    function mint(uint256 shipType, uint256 amount)
+    
+
+    // Function to update ship abilities
+    function updateShipAbilities(uint256 _id, string[] memory _abilities) public onlyRole(MINTER_ROLE) {
+        if (shipAbilities[_id].length == 0) {
+            shipAbilities[_id] = _abilities;
+        } else {
+            shipAbilities[_id] = _abilities;
+        }
+    }
+
+    // Function to get premium ship abilities
+    function getPremiumShipAbilities(uint256 _id) public view returns (string[] memory) {
+        return shipAbilities[_id];
+    }
+
+
+    //@notice This mints fungible tokens
+    function mintFree()
         public
     {
-        //require(navisToken.transferFrom(msg.sender, feeCollector, MINT_PRICE * amount), "Fee transfer failed");
-        require(shipType > 0 && shipType <= 75, "Invalid ship type");
-        _mint(msg.sender, shipType, amount, "");
-        _setURI(shipTypeURIs[shipType]); // Set the URI for the specific ship type
+        require(!userHasMinted[msg.sender], "User already minted free NFT");
+        userHasMinted[msg.sender] = true;
+        for (uint256 i = 0; i < 6; i++) 
+        {
+            _mint(msg.sender, i, 1, "");
+        _setURI(shipTypeURIs[i]); // Set the URI for the specific ship type
+        }        
     }
+
+    //@notice This mints non-fungible tokens
+    function mintPremium(uint256 shipType)
+        public
+    {
+        require(navisToken.transferFrom(msg.sender, feeCollector, MINT_PRICE), "Fee transfer failed");
+        require(shipType > 5 && shipType <= 75, "Invalid ship type");
+        uint256 newTokenId = _tokenIdTracker.current() + PREMIUM_ID_OFFSET;
+        _mint(msg.sender, newTokenId, 1, "");
+        _setURI(shipTypeURIs[shipType]); 
+        _tokenIdTracker.increment(); 
+    }
+
 
     function setShipTypeURI(uint256 shipType, string memory uri) public onlyRole(URI_SETTER_ROLE) {
         require(shipType > 0 && shipType <= 50, "Invalid ship type");
         shipTypeURIs[shipType] = uri;
     }
-
    
-
-
     // Helper function to convert uint256 to string
     function uint2str(uint256 _i) internal pure returns (string memory _uintAsString) {
         if (_i == 0) {
